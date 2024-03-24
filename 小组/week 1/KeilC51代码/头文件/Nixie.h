f29611812312f11017c00aabab059c8ce9b12167
zV1per <774065051@qq.com>
2024-03-24 11:47:12 +0800

    11

---
+++ 小组/week 1/KeilC51代码/头文件/Nixie.h
@@ -0,0 +1,7 @@
+#ifndef __NIXIE_H__
+#define __NIXIE_H__
+#include <REGX52.h>
+
+void Nixie(unsigned char Location,Number);
+
+#endif
---
+++ 小组/week 1/KeilC51代码/源文件/mian.c
@@ -0,0 +1,30 @@
+#include <REGX52.h>
+#include "Delay.h"
+#include <INTRINS.H>
+#include "Led.h"
+#include "Buzzer.h"
+#include "Nixie.h"
+
+
+void main()
+{
+	unsigned int i = 0;
+	while(1)
+	{	
+//		Buzzer(100);
+		
+		
+//		Nixie(1,1);
+//		Nixie(2,2);
+//		Nixie(3,3);
+//		Nixie(4,4);
+		
+		
+//		for(i=0;i<8;i++)
+//		{
+//			Led(0x01<<i);Delay(500);
+//		}
+		
+		
+	}
+}
